740c81c836b3fd848a7420b41ad50b85ce107d96
Nils Haberkamp <haberkamp.n@gmail.com>
2023-10-26 17:15:57 +0000

    create proof of concept

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,74 @@
-const { program } = require('commander');
+import { cruise, type IModule, type IReporterOutput } from 'dependency-cruiser';
 
-program
-  .option('--first')
-  .option('-s, --separator <char>');
+const cruiseResult: IReporterOutput = await cruise(['src'], {
+  includeOnly: '^src/'
+});
 
-program.parse();
+if (typeof cruiseResult.output === 'string') throw new Error('lul');
 
-const options = program.opts();
-const limit = options.first ? 1 : undefined;
+type FileResult = {
+  // path to the file
+  source: string;
+  // the amount of dependencies the files has
+  dependencies: number
+  // the amount of files that are dependent on this file
+  dependents: number
+}
 
-console.log(program.args[0].split(options.separator, limit));
+function countDependencies(module: IModule): string[] {
+
+  return module.dependencies.reduce<string[]>((accumulator, dependency) => {
+    if (typeof cruiseResult.output === 'string') throw new Error('lul');
+    const dependenciesOfDependency = cruiseResult.output.modules.filter(currentModule => currentModule.source === dependency.resolved);
+
+    const result = dependenciesOfDependency.reduce<string[]>((accumulator, currentModule) => {
+      const deps = countDependencies(currentModule);
+
+      return [...accumulator, ...deps];
+    }, []);
+
+    return [...accumulator, dependency.resolved, ...result];
+  }, []);
+}
+
+function countDependents(module: IModule): string[] {
+  return module.dependents.reduce<string[]>((accumulator, dependent) => {
+    if (typeof cruiseResult.output === 'string') throw new Error('lul');
+    const dependentsOfDependent = cruiseResult.output.modules.filter(currentModule => currentModule.source === dependent);
+
+    const result = dependentsOfDependent.reduce<string[]>((accumulator, currentModule) => {
+      const deps = countDependents(currentModule);
+
+      return [...accumulator, ...deps];
+    }, []);
+
+    return [...accumulator, ...result, dependent];
+  }, []);
+}
+
+const result = cruiseResult.output.modules.reduce<Array<FileResult>>((accumulator, moduleReport) => {
+  const deps = countDependencies(moduleReport);
+  const dependents = countDependents(moduleReport);
+
+  const depsCount = new Set(deps).size;
+  const dependentsCount = new Set(dependents).size;
+
+  return [...accumulator, {
+    source: moduleReport.source,
+    dependencies: depsCount,
+    dependents: dependentsCount
+  }];
+}, []).sort((a, b) => {
+  // 1. sort by dependencies (ascending)
+  // 2. sort by dependents (descending)
+  // 3. sort by source (ascending)
+  if (a.dependencies > b.dependencies) return 1;
+  if (a.dependencies < b.dependencies) return -1;
+
+  if (a.dependents > b.dependents) return -1;
+  if (a.dependents < b.dependents) return 1;
+
+  return a.source.localeCompare(b.source);
+});
+
+console.table(result);
