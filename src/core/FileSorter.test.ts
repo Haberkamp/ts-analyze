1fabbc6e4ad45b95d6d1cf47c835c4db39b8384c
Nils Haberkamp <haberkamp.n@gmail.com>
2023-11-02 14:42:03 +0100

    sort files by the number of their dependencies in an ascending order

--- src/core/FileSorter.test.ts
+++ src/core/FileSorter.test.ts
@@ -1,7 +1,42 @@
-import { describe, it } from 'vitest';
+import { describe, it, expect } from 'vitest';
+import FileSorter from './FileSorter.js';
+import type { FileResult } from '../types/fileResult.js';
 
 describe('src/core/FileSorter.ts', () => {
-	it.todo('should sort files by their number of dependencies (ascending)');
+	it('should sort files by their number of dependencies (ascending)', () => {
+		// ARRANGE
+		const files: FileResult[] = [
+			{
+				source: 'b.js',
+				dependencies: 1,
+				dependents: 0,
+			},
+			{
+				source: 'a.js',
+				dependencies: 0,
+				dependents: 0,
+			},
+		];
+
+		const sorter = new FileSorter();
+
+		// ACT
+		const result = sorter.sort(files);
+
+		// ASSERT
+		expect(result).toStrictEqual([
+			{
+				source: 'a.js',
+				dependencies: 0,
+				dependents: 0,
+			},
+			{
+				source: 'b.js',
+				dependencies: 1,
+				dependents: 0,
+			},
+		]);
+	});
 
 	it.todo('should sort files by their number of dependents (descending)');
 
--- src/core/FileSorter.ts
+++ src/core/FileSorter.ts
@@ -2,7 +2,13 @@ import type FileSorterInterface from '@/src/core/FileSorterInterface.ts';
 import type { FileResult } from '../types/fileResult.ts';
 
 export default class FileSorter implements FileSorterInterface {
-	sort(unsortedFiles: FileResult[]) {
-		throw new Error('method sort() not implemented.');
+	sort(unsortedFiles: FileResult[]): FileResult[] {
+		return unsortedFiles.sort((firstFile, secondFile) => {
+			// return by their number of dependencies (ascending)
+			if (firstFile.dependencies < secondFile.dependencies) return -1;
+			if (firstFile.dependencies > secondFile.dependencies) return 1;
+
+			return 0;
+		});
 	}
 }
